4a46595c4cb2f6274bac362aa9740b86020ea8cc
hasnat amir <hasnatamir2@gmail.com>
2020-11-06 17:18:05 +0500

    Updates

---
+++ precache-manifest.3c30e67845789281295f6f09db186d54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90721edf459dc92594427d12f0fec00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f879a97546a7ffcb0bd",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "4b8b20d4fef1b9022fbe",
+    "url": "/static/css/main.8693fa7e.chunk.css"
+  },
+  {
+    "revision": "3f879a97546a7ffcb0bd",
+    "url": "/static/js/2.df6e5d6f.chunk.js"
+  },
+  {
+    "revision": "99f40b8c879c41e79e2e4a939ebf6241",
+    "url": "/static/js/2.df6e5d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8b20d4fef1b9022fbe",
+    "url": "/static/js/main.34cde50d.chunk.js"
+  },
+  {
+    "revision": "d261b5d858b46049713c",
+    "url": "/static/js/runtime-main.c10f071b.js"
+  },
+  {
+    "revision": "088eb6cca469ec3f7ed6c9b1f9576d71",
+    "url": "/static/media/Jahangiri1.088eb6cc.png"
+  },
+  {
+    "revision": "e5aa1248325c27673557a0615fdeefa1",
+    "url": "/static/media/hasnatAmir.e5aa1248.JPG"
+  },
+  {
+    "revision": "d20bc7d35c16478fb35cee1efa16121b",
+    "url": "/static/media/home_create.d20bc7d3.png"
+  }
+]);
