2ea23c6daebc39e2d6a1a7c2fc05c070fc004351
hasnatamir2 <hasnatamir2@gmail.com>
2026-02-20 00:26:24 +0400

    fix: hopefully

--- src/app/actions/sendContact.ts
+++ src/app/actions/sendContact.ts
@@ -9,7 +9,7 @@ export async function sendContact(_: unknown, formData: FormData) {
       message: formData.get('message'),
     }
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/send`, {
+    const res = await fetch(`https://hasnatdev.com/api/send`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
